19d8f80b945415afe4d17adaf6e945f392e4274a
Chris Sewell <chrisj_sewell@hotmail.com>
2017-07-11 20:30:41 +0100

    added some extra options and recording of latexmk log

--- run_nbconvert.sh
+++ run_nbconvert.sh
@@ -4,11 +4,10 @@ bib_path=""
 logo_path=""
 output_format="latex_ipypublish_main.py"
 nconvert_path="nbconvert"
+create_pdf=true
+copy_images=false
 
-# bib_path=${2:-"bibliographies/example.bib"}
-# logo_path=${3:-"logos/logo_example.png"}
-
-usage="usage: run_nbconvert [-h -b -l -f —n] ipynb_path 
+usage="usage: run_nbconvert [-h -b -l -f —n -p] ipynb_path 
 program to convert jupyter notebooks to tex and pdf format
 
 where:
@@ -18,10 +17,12 @@ where:
     -l  path to title page logo
     -f  output format file to use (default: ${output_format})
     -n  path to nbconvert folder (default: ${nconvert_path})
+	-p  true/false create pdf (if converting to latex)  (default: ${create_pdf})
+	-i  true/false copy image folder to output (if converting to latex) (default: ${copy_images})
 "
 
 OPTIND=1
-while getopts ':hb:l:f:n:' option; do
+while getopts ':hb:l:f:n:p:i:' option; do
   case "$option" in
     h) echo "$usage"
        return 0
@@ -34,6 +35,10 @@ while getopts ':hb:l:f:n:' option; do
        ;;
     n) nconvert_path=$OPTARG
        ;;
+    p) create_pdf=$OPTARG
+      ;;
+    i) copy_images=$OPTARG
+        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
        return 1
@@ -94,7 +99,7 @@ rm -r "${output_format}"
 
 if [ ! -e "${ipynb_name}.tex" ]; then
 	if [ ! -e "${ipynb_name}.html" ]; then	
-		echo "Error: no latex or html file created"
+		echo "Error: no latex or html file created, check files in ${nconvert_path} for issues"
 		cd -
 		exit
 	else
@@ -103,20 +108,31 @@ if [ ! -e "${ipynb_name}.tex" ]; then
 			mkdir converted
 		fi
 		cp "${nconvert_path}/${ipynb_name}.html"  "converted/${ipynb_name}.html"
-		echo "output copied to: converted/${ipynb_name}.html"
+		echo "Finished: output copied to: converted/${ipynb_name}.html"
 				
 	fi
 else
-	latexmk -bibtex -pdf "${ipynb_name}.tex"
-	cd -
 
 	if [ ! -d "converted" ]; then
 		mkdir converted
 	fi
-	cp "${nconvert_path}/${ipynb_name}.pdf"  "converted/${ipynb_name}.pdf"
+
+	if [ "${create_pdf}" = true ] ; then
+		latexmk -bibtex -pdf "${ipynb_name}.tex" | tee latex_to_pdf.log
+		cd -
+		cp "${nconvert_path}/latex_to_pdf.log"  "converted/${ipynb_name}.latex_to_pdf.log"
+		sed 's/{name_of_pdf_here}/'"${ipynb_name}.pdf"'/' < "${nconvert_path}/scripts/view_pdf.html" > "converted/${ipynb_name}_viewpdf.html"
+		cp "${nconvert_path}/${ipynb_name}.pdf"  "converted/${ipynb_name}.pdf"
+	else
+		cd -
+    fi
+	
 	cp "${nconvert_path}/${ipynb_name}.tex"  "converted/${ipynb_name}.tex"
-	sed 's/{name_of_pdf_here}/'"${ipynb_name}.pdf"'/' < "${nconvert_path}/scripts/view_pdf.html" > "converted/${ipynb_name}_viewpdf.html"
-	echo "output copied to: converted/${ipynb_name}.*"
+	if [ "${copy_images}" = true ] ; then
+		rm -rf "converted/${ipynb_name}_files"
+		cp -r "${nconvert_path}/${ipynb_name}_files" converted
+	fi
+	echo "Finished: output copied to: converted/${ipynb_name}.*"
 
 	rm -rf "${nconvert_path}/${ipynb_name}_files"	
 fi
