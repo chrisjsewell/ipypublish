711e073fff02c419d836701afcd6c1643d60e9a7
Chris Sewell <chrisj_sewell@hotmail.com>
2017-07-17 03:29:07 +0100

    improved argparsing (added available exporters in help)

--- ipypublish/test_main.py
+++ ipypublish/test_main.py
@@ -100,6 +100,93 @@ class Test_main(object):
      "nbformat": 4,
      "nbformat_minor": 2
 }""")
+
+        self.file_with_bib = MockPath('test_with_bib.ipynb',is_file=True,
+        content=r"""{
+ "cells": [
+      {
+       "cell_type": "markdown",
+       "metadata": {},
+       "source": ["citation.\\cite{zelenyak_molecular_2016}"]
+      },
+      {
+       "cell_type": "code",
+       "execution_count": 2,
+       "metadata": {},
+       "source": [
+            "a=1\n",
+            "print(a)"
+           ],
+           "outputs": [
+            {
+             "name": "stdout",
+             "output_type": "stream",
+             "text": ["1\n"]
+            }
+           ]
+      }
+     ],
+ "metadata": {
+      "test_name": "notebook2",
+      "latex_doc": {"bibliography":"test.bib"},
+      "kernelspec": {
+       "display_name": "Python 3",
+       "language": "python",
+       "name": "python3"
+      },
+      "language_info": {
+       "codemirror_mode": {
+        "name": "ipython",
+        "version": 3
+       },
+       "file_extension": ".py",
+       "mimetype": "text/x-python",
+       "name": "python",
+       "nbconvert_exporter": "python",
+       "pygments_lexer": "ipython3",
+       "version": "3.6.1"
+      }},
+     "nbformat": 4,
+     "nbformat_minor": 2
+}""")
+
+        self.bibfile = MockPath('test.bib',is_file=True,
+        content=r"""
+@article{kirkeminde_thermodynamic_2012,
+  title = {Thermodynamic Control of Iron Pyrite Nanocrystal Synthesis with High Photoactivity and Stability},
+  volume = {1},
+  issn = {2050-7496},
+  doi = {10.1039/C2TA00498D},
+  abstract = {Non-toxic, earth abundant nanostructured semiconductors have received extensive attention recently. One of the more highly studied materials has been iron pyrite (FeS2) due to its many different promising applications. Herein, we report the thermodynamically-controlled synthesis of FeS2 nanocrystals, dependent on the reaction temperature and chemical precursors, and a Lewis acid/base model to explain the shape-controlled synthesis. The surface facet-controlled photocatalytic activity and photostability were studied and explained. This work further advances the synthesis with pyrite structure control and surface facet-dictated applications, such as photovoltaics, photocatalysts and photoelectrochemical cells.},
+  timestamp = {2017-07-06T00:26:10Z},
+  langid = {english},
+  number = {1},
+  journaltitle = {Journal of Materials Chemistry A},
+  shortjournal = {J. Mater. Chem. A},
+  author = {Kirkeminde, Alec and Ren, Shenqiang},
+  urldate = {2017-06-18},
+  date = {2012-11-29},
+  pages = {49--54}
+}
+
+@article{zelenyak_molecular_2016,
+  title = {Molecular Dynamics Study of Perovskite Structures with Modified Interatomic Interaction Potentials},
+  volume = {50},
+  issn = {0018-1439, 1608-3148},
+  doi = {10.1134/S0018143916050209},
+  abstract = {The structure of compounds with the perovskite structure ABX3.},
+  timestamp = {2017-07-06T00:19:33Z},
+  langid = {english},
+  number = {5},
+  journaltitle = {High Energy Chemistry},
+  shortjournal = {High Energy Chem},
+  author = {Zelenyak, T. Yu and Kholmurodov, Kh T. and Tameev, A. R. and Vannikov, A. V. and Gladyshev, P. P.},
+  urldate = {2017-07-06},
+  date = {2016-09-01},
+  pages = {400--405}
+}
+""")
+
         self.directory = MockPath('dir1',structure=[self.file1,self.file2])
         
     def test_nbmerge_one_notebook(self):
@@ -227,6 +314,19 @@ test123
             assert os.path.exists(pdf_path)
         finally:
             shutil.rmtree(out_folder)    
+
+    # def test_publish_withbib(self):
+    #     out_folder = tempfile.mkdtemp()
+    #     tex_path = os.path.join(out_folder,'test_with_bib.tex')
+    #     pdf_path = os.path.join(out_folder,'test_with_bib.pdf')
+    #     with self.file_with_bib.maketemp() as file_with_bib_dir:
+    #
+    #         try:
+    #             publish(file_with_bib_dir.name,outpath=out_folder,create_pdf=True)
+    #             assert os.path.exists(tex_path)
+    #             assert os.path.exists(pdf_path)
+    #         finally:
+    #             shutil.rmtree(out_folder)
 
     def test_publish_file1_html(self):
         
