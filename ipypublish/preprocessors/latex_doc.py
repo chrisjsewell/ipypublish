fbc5cced30b8b7de3172898dbd51ddeecc2f9ed7
Chris Sewell <chrisj_sewell@hotmail.com>
2017-07-20 20:47:05 +0100

    try not extracting caption cells

--- ipypublish/preprocessors/latex_doc.py
+++ ipypublish/preprocessors/latex_doc.py
@@ -72,25 +72,25 @@ class LatexDocLinks(Preprocessor):
         
         logging.info('extracting caption cells') 
         
-        # extract captions
-        final_cells = []
-        captions = {}
-        for cell in nb.cells:
-            if hasattr(cell.metadata, 'latex_doc'):
-                if hasattr(cell.metadata.latex_doc, 'caption'):
-                    captions[cell.metadata.latex_doc.caption] = cell.source.split(r'\n')[0]
-                    continue
-            final_cells.append(cell)
-        nb.cells = final_cells  
-        
-        # replace captions
-        for cell in nb.cells:
-            if hasattr(cell.metadata, 'latex_doc'):
-                for key in cell.metadata.latex_doc:
-                    if hasattr(cell.metadata.latex_doc[key], 'label'):
-                        if cell.metadata.latex_doc[key]['label'] in captions:
-                            logging.debug('replacing caption for: {}'.format(cell.metadata.latex_doc[key]['label']))
-                            cell.metadata.latex_doc[key]['caption'] = captions[cell.metadata.latex_doc[key]['label']]
+        # # extract captions
+        # final_cells = []
+        # captions = {}
+        # for cell in nb.cells:
+        #     if hasattr(cell.metadata, 'latex_doc'):
+        #         if hasattr(cell.metadata.latex_doc, 'caption'):
+        #             captions[cell.metadata.latex_doc.caption] = cell.source.split(r'\n')[0]
+        #             continue
+        #     final_cells.append(cell)
+        # nb.cells = final_cells
+        #
+        # # replace captions
+        # for cell in nb.cells:
+        #     if hasattr(cell.metadata, 'latex_doc'):
+        #         for key in cell.metadata.latex_doc:
+        #             if hasattr(cell.metadata.latex_doc[key], 'label'):
+        #                 if cell.metadata.latex_doc[key]['label'] in captions:
+        #                     logging.debug('replacing caption for: {}'.format(cell.metadata.latex_doc[key]['label']))
+        #                     cell.metadata.latex_doc[key]['caption'] = captions[cell.metadata.latex_doc[key]['label']]
                 
         
         return nb, resources
