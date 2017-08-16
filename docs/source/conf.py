7580ccc0fbfa5ec68eb87b7cbd5760fef1b039dc
Chris Sewell <chrisj_sewell@hotmail.com>
2017-08-16 03:01:12 +0100

    change icon size

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,7 +126,7 @@ todo_include_todos = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = '_static/doc_icon.png'
+html_logo = '_static/doc_icon_100px.png'
 
 # -- Options for HTMLHelp output ------------------------------------------
 
