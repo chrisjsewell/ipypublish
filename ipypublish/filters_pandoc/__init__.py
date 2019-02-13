f13d7d7dcaa3076d18c937273db82c2ec0b3f1cc
Chris Sewell <chrisj_sewell@hotmail.com>
2019-02-13 21:15:52 +0000

    monkey patch failing panflute function

--- ipypublish/filters_pandoc/__init__.py
+++ ipypublish/filters_pandoc/__init__.py
@@ -1,8 +1,34 @@
 """
 pandoc filters for the ipypublish format
 """
+import sys
+from six import string_types
 import panflute as pf
 pf.elements.RAW_FORMATS.add("latex")
 pf.elements.RAW_FORMATS.add("tex")
 pf.elements.RAW_FORMATS.add("rst")
 pf.elements.RAW_FORMATS.add("html5")
+
+
+# see https://github.com/sergiocorreia/panflute/issues/97
+# TODO delete this when fixed
+if sys.version_info[0] == 2:
+    def builtin2meta(val):
+        if isinstance(val, bool):
+            return pf.MetaBool(val)
+        elif isinstance(val, (float, int)):
+            return pf.MetaString(str(val))
+        elif isinstance(val, string_types):
+            return pf.MetaString(val)
+        elif isinstance(val, list):
+            return pf.MetaList(*[builtin2meta(x) for x in val])
+        elif isinstance(val, dict):
+            return pf.MetaMap(*[(k, builtin2meta(v)) for k, v in val.items()])
+        elif isinstance(val, pf.Block):
+            return pf.MetaBlocks(val)
+        elif isinstance(val, pf.Inline):
+            return pf.MetaInlines(val)
+        else:
+            return val
+
+    pf.elements.builtin2meta = builtin2meta
