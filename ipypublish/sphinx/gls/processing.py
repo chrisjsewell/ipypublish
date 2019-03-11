11de1c29c950e88510af39d35de051f138299264
Chris Sewell <chrisj_sewell@hotmail.com>
2019-03-11 13:30:27 +0000

    fix label

--- ipypublish/sphinx/gls/processing.py
+++ ipypublish/sphinx/gls/processing.py
@@ -36,23 +36,22 @@ def process_citations(app, doctree, docname):
     :param docname: The document name.
     :type docname: :class:`str`
     """
+    # citation(rawsource='', *children, **attributes)
     for node in doctree.traverse(docutils.nodes.citation):
         if "bibglossary" not in node.attributes.get('classes', []):
             continue
         key = node[0].astext()
         try:
-            label = app.env.bibgloss_cache.get_label_from_key(key)
+            label_str = app.env.bibgloss_cache.get_label_from_key(key)
         except KeyError:
             logger.warning("could not relabel glossary item [%s]" % key)
         else:
             if app.config.bibgloss_convert_latex:
-                label = latex_to_docutils(label)
-                # TODO this doesn't work because the label turns
-                # all the elements into text
-                # e.g. \textbf{other} -> <strong>other</strong>
-                node[0] = docutils.nodes.label('', *label.children[0])
+                label = latex_to_docutils(label_str)
+                # label(rawsource='', text='', *children, **attributes)
+                node[0] = docutils.nodes.label('', '', *label.children[0])
             else:
-                node[0] = docutils.nodes.label('', label)
+                node[0] = docutils.nodes.label('', label_str)
 
 
 def process_citation_references(app, doctree, docname):
@@ -66,6 +65,7 @@ def process_citation_references(app, doctree, docname):
     """
     # sphinx has already turned citation_reference nodes
     # into reference nodes, so iterate over reference nodes
+    # reference(rawsource='', text='', *children, **attributes)
     for node in doctree.traverse(docutils.nodes.reference):
         if "bibglossary" not in node.attributes.get('classes', []):
             continue
