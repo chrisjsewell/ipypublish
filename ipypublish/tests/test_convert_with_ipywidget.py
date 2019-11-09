9f57ff25901eca62abf7951787ba7616f59969a6
Chris Sewell <chrisj_sewell@hotmail.com>
2019-11-09 21:47:45 +0000

    skip test on python 3.5

--- ipypublish/tests/test_convert_with_ipywidget.py
+++ ipypublish/tests/test_convert_with_ipywidget.py
@@ -1,6 +1,9 @@
+import sys
+
 import pytest
 
 
+@pytest.mark.skipif(sys.version_info[:2] == (3, 5), reason='json_dumps puts keys in different order')
 @pytest.mark.ipynb('nb_with_ipywidget')  # , out_to_temp=False)
 def test_ipywidget_sphinx_rst(ipynb_app):
     """The notebook contains an ipywidgets.Button and the widget state has been saved."""
